f9298f4ba7674d3b3b43b48f65a4e767643d2fea
Christopher Coffee <chrisc1891@gmail.com>
2021-06-02 15:12:59 -0500

    Time: 440 ms (5.04%), Space: 52.5 MB (5.04%) - LeetHub

---
+++ missing-number/missing-number.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun missingNumber(nums: IntArray): Int {
+       nums.sort()
+        
+        for(i in 0 until nums.size){
+            if(nums[i] != i) return i
+        }
+        
+        return nums.size
+    }
+}

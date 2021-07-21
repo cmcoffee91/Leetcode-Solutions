a1786f035824de12208bbbedd2b767e7caa0f28d
Christopher Coffee <chrisc1891@gmail.com>
2021-07-20 20:28:52 -0500

    Time: 0 ms (100.00%), Space: 38.8 MB (40.57%) - LeetHub

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,14 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        
         int max = 0;
-        int left = 0;
         for(int i = 1;i < prices.length;i++){
-            int currentDay = prices[left];
-            int nextDay = prices[i];
-            if(nextDay > currentDay) max += nextDay - currentDay;
-            left++;
+            int currentPrice = prices[i-1];
+            int nextDayPrice = prices[i];
+            if(nextDayPrice > currentPrice) max += nextDayPrice - currentPrice; 
         }
         
         return max;
+
     }
 }

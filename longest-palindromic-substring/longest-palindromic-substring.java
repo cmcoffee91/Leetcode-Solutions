dfdad1131cb32ee7a6ac797844da7a4ee9027ebd
Christopher Coffee <chrisc1891@gmail.com>
2021-08-02 03:14:14 -0500

    Time: 8 ms (97.85%), Space: 39.4 MB (52.86%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.java
+++ longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,28 +1,33 @@
 class Solution {
     public String longestPalindrome(String s) {
+        
         char[] sArray = s.toCharArray();
-        int left = 0, right = 0;
+        int start = 0;
+        int end = 0;
+        
         for(int i = 0;i < sArray.length;i++){
-           int lengthOne = expandAroundCenter(sArray, i, i); 
-           int lengthTwo = expandAroundCenter(sArray, i, i+1); 
-           int longest = Math.max(lengthOne, lengthTwo);
             
-           if(longest > right - left){
-               left = i - ((longest - 1) / 2);
-               right = i + (longest / 2);
-           }
+            int lengthOne = expandAroundCenter(sArray,i, i);
+            int lengthTwo = expandAroundCenter(sArray,i,i+1);
+            int longest = Math.max(lengthOne, lengthTwo);
+            
+            if(longest > end - start ){
+                start = i - (longest - 1)/2;
+                end = i + longest/2;
+            }
+            
         }
-       return s.substring(left, right + 1);
+
+        return s.substring(start,end+1);
     }
     
     public int expandAroundCenter(char[] sArray, int left, int right){
-        while (left >= 0 && right < sArray.length && sArray[left] == sArray[right]){
-            right++;
-            left--;
+        int r = right;
+        int l = left;
+        while(l >= 0 && r < sArray.length && sArray[l] == sArray[r]){
+            l--;
+            r++;
         }
-        return right - left - 1;
+        return r - l - 1;
     }
-    
-    
-    
 }

42f27146640c6e4ceadb6fd6e620e7de21e3139e
Christopher Coffee <chrisc1891@gmail.com>
2021-08-08 02:11:09 -0500

    Time: 204 ms (65.75%), Space: 37.5 MB (66.31%) - LeetHub

--- two-sum/two-sum.kt
+++ two-sum/two-sum.kt
@@ -1,23 +1,20 @@
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        var map = HashMap<Int,Int>()
         
+        val map = HashMap<Int, Int>()
         for(i in 0 until nums.size){
-            var complement = target - nums[i]
-            map?.let{
-               if(it.containsKey(complement)){
-                    it[complement]?.let{ myVal ->
-                        return intArrayOf(i, myVal)
-                    }
-                }
-                else{
-                    it[nums[i]] = i
+            val complement = target - nums[i]
+            if(map.containsKey(complement)){
+                map[complement]?.let{ index -> 
+                    return intArrayOf(i, index )
                 }
             }
-            
+            else{
+                map.put(nums[i], i)
+            }
         }
         
-        return IntArray(2)
-        
+        return intArrayOf(-1, -1)
+
     }
 }

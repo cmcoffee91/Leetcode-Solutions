ca0d8b97417289c29ad2b4a9811b6f117b4b4e51
Christopher Coffee <chrisc1891@gmail.com>
2021-08-20 14:50:41 -0500

    Time: 268 ms (16.94%), Space: 41.4 MB (70.14%) - LeetHub

--- add-two-numbers/add-two-numbers.kt
+++ add-two-numbers/add-two-numbers.kt
@@ -9,27 +9,26 @@
  */
 class Solution {
     fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+        val dummy = ListNode(-1)
+        var current = dummy 
+        var tmpOne = l1
+        var tmpTwo = l2
+        
         var carry = 0
-        var dummy = ListNode(-1)
-        var current = dummy
-        
-        var tmpL1 = l1
-        var tmpL2 = l2
-        
-        while(tmpL1 != null || tmpL2 != null){
-            val one = if(tmpL1 != null) tmpL1.`val` else 0
-            val two = if(tmpL2 != null) tmpL2.`val` else 0
+        while(tmpOne != null || tmpTwo != null){
+            val one = if (tmpOne != null) tmpOne.`val` else 0
+            val two = if (tmpTwo != null) tmpTwo.`val` else 0
             val sum = one + two + carry
             carry = sum / 10
+            println(carry)
             current.next = ListNode(sum % 10)
-            if(tmpL1 != null)tmpL1 = tmpL1.next
-            if(tmpL2 != null)tmpL2 = tmpL2.next
             current = current.next
+            if(tmpOne != null)tmpOne = tmpOne.next
+            if(tmpTwo != null)tmpTwo = tmpTwo.next
         }
         
         if(carry > 0) current.next = ListNode(carry)
+        
         return dummy.next
-        
-        
     }
 }

0e15ca9162a5fcbd4299e03e9c7250a262908bc4
Christopher Coffee <chrisc1891@gmail.com>
2021-08-23 08:58:56 -0500

    Time: 156 ms (83.20%), Space: 35.3 MB (18.40%) - LeetHub

---
+++ remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.kt
@@ -0,0 +1,32 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+        var dummy = ListNode(-1)
+        dummy.next = head
+        
+        var tmp = dummy.next
+        var size = 0
+        while( tmp != null ) {
+            tmp = tmp.next
+            size++
+        }
+        
+        size -= n
+        tmp = dummy
+        while(size > 0){
+            tmp = tmp.next
+            size--
+        }
+        tmp.next = tmp.next.next
+        return dummy.next
+        
+    }
+}

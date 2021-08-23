5ed7cdce59ef1de14d4c02ccdbcd8c31126f8adc
Christopher Coffee <chrisc1891@gmail.com>
2021-08-23 01:28:45 -0500

    Time: 196 ms (75.26%), Space: 36.9 MB (66.67%) - LeetHub

--- valid-palindrome/valid-palindrome.kt
+++ valid-palindrome/valid-palindrome.kt
@@ -1,25 +1,31 @@
 class Solution {
     fun isPalindrome(s: String): Boolean {
+        
         val sArray = s.toLowerCase().toCharArray()
+        
         var left = 0
-        var right = sArray.size-1
-        while(left < right){
-           val first = sArray[left];
-           val second = sArray[right];
-           if( !first.isLetterOrDigit() )  {
-               left++
-               continue
-           }
-           else if( !second.isLetterOrDigit() )  {
-               right--
-               continue
-           }
-           else if( first != second ){
-               return false
-           }
-           left++
-           right--
+        var right = sArray.size - 1
+        
+        while(left < right) {
+            val leftVal = sArray[left]
+            val rightVal = sArray[right]
+            
+            if( !leftVal.isLetterOrDigit() ){
+                left++
+                continue
+            }
+            else if( !rightVal.isLetterOrDigit() ){
+                right--
+                continue
+            }
+            else if(leftVal != rightVal) {
+               return false 
+            }
+            left++
+            right--
         }
+        
         return true
+        
     }
 }

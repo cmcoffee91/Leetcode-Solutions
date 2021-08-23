57a40c6783ba2c13b401085e283f4268212db815
Christopher Coffee <chrisc1891@gmail.com>
2021-08-22 23:38:10 -0500

    Time: 148 ms (56.14%), Space: 33.3 MB (91.63%) - LeetHub

--- reverse-integer/reverse-integer.kt
+++ reverse-integer/reverse-integer.kt
@@ -1,12 +1,13 @@
 class Solution {
     fun reverse(x: Int): Int {
-        var result = 0L 
+        var result = 0 
         var tmp = x
         while(tmp != 0){
             val remainder = tmp % 10
-            result = result * 10 + remainder.toLong()
-            if(result < Int.MIN_VALUE || result > Int.MAX_VALUE) return 0
+            if(result > Int.MAX_VALUE / 10 || result == Int.MAX_VALUE / 10 && remainder > 7) return 0
+            if(result < Int.MIN_VALUE / 10 || result == Int.MIN_VALUE / 10 && remainder < -8) return 0
             tmp /= 10 
+            result = result * 10 + remainder
         }
         
         return result.toInt()

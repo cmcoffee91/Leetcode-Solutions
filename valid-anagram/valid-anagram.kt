12f05c41c89fb7e339bb4eb29c03913bbdb4334a
Christopher Coffee <chrisc1891@gmail.com>
2021-08-23 01:07:46 -0500

    Time: 212 ms (43.17%), Space: 36.1 MB (57.27%) - LeetHub

--- valid-anagram/valid-anagram.kt
+++ valid-anagram/valid-anagram.kt
@@ -1,40 +1,24 @@
 class Solution {
     fun isAnagram(s: String, t: String): Boolean {
-        
-        if(s.length != t.length) return false
-        val sArray = s.toCharArray()
-        val tArray = t.toCharArray()
-        
-        val map = HashMap<Char, Int>()
-        
-        for(i in 0 until sArray.size){
-            if(map.containsKey(sArray[i])){
-              var current = map[sArray[i]]  
-              current?.let{
-                  var updateNum = it
-                  map[sArray[i]] = ++updateNum
-              }
-            }
-            else{
-              map[sArray[i]] = 1
-            }
-        }
-        
-         for(i in 0 until tArray.size){
-             if(map.containsKey(tArray[i])){
-                 var current = map[tArray[i]]
-                 current?.let{
-                  var updateNum = it
-                  updateNum--
-                  if(updateNum < 0) return false
-                  map[tArray[i]] = updateNum
-                 }
-             }
+         val map = HashMap<Char, Int>()
+         val sArray = s.toCharArray()
+         val tArray = t.toCharArray()
+         
+         if(sArray.size != tArray.size) return false
+         
+         for(c in sArray){
+             map[c] = map.getOrDefault(c, 0) + 1
+         }
+         
+          for(c in tArray){
+             var current = map.getOrDefault(c, 0)
+             if(current  > 0){
+               map[c] = --current  
+             } 
              else{
                  return false
              }
          }
-         
         
         return true
     }

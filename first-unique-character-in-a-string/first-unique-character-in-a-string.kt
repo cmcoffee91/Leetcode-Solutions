362bef9dbf1601e3d139cc085a371affa5a497da
Christopher Coffee <chrisc1891@gmail.com>
2021-08-23 00:58:20 -0500

    Time: 284 ms (51.01%), Space: 36.6 MB (51.01%) - LeetHub

--- first-unique-character-in-a-string/first-unique-character-in-a-string.kt
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.kt
@@ -1,29 +1,16 @@
 class Solution {
     fun firstUniqChar(s: String): Int {
+        val map = HashMap<Char, Int>()
         
-        val map = HashMap<Char,Int>()
         val sArray = s.toCharArray()
-        for(i in 0 until sArray.size){
-            
-            if( map.containsKey( sArray[i] ) ) {
-                map[ sArray[i] ]?.let{ myVal ->
-                    var currentCount = myVal
-                    map[ sArray[i] ] = ++currentCount
-                }
-                
-            }
-            else
-            {
-                map.put(sArray[i],1)
-            }
+        for(c in sArray){
+            map[c] = map.getOrDefault(c, 0) + 1
         }
         
         for(i in 0 until sArray.size){
-            val count = map[ sArray[i] ]
-            if(count == 1) return i
+            if( map[ sArray[i] ] == 1 ) return i
         }
         
         return -1
-        
     }
 }

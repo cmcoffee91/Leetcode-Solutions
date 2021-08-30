047ce8b80f5e0f6f9f578804c7d7e03625c37f2f
Christopher Coffee <chrisc1891@gmail.com>
2021-08-30 00:39:38 -0500

    Time: 241 ms (5.48%), Space: 34.1 MB (12.33%) - LeetHub

--- middle-of-the-linked-list/middle-of-the-linked-list.kt
+++ middle-of-the-linked-list/middle-of-the-linked-list.kt
@@ -10,14 +10,13 @@
 class Solution {
     fun middleNode(head: ListNode?): ListNode? {
         
-        var fast = head
         var slow = head
+        var fast = head
         while(fast != null && fast?.next != null){
-            fast = fast?.next?.next
             slow = slow?.next
+            fast = fast?.next?.next
         }
-        
+      
         return slow
-        
     }
 }

21a7fadebeefde4319f52c7fa8cac32b70007749
Christopher Coffee <chrisc1891@gmail.com>
2021-09-25 02:59:55 -0500

    Time: 0 ms (100.00%), Space: 39.9 MB (74.97%) - LeetHub

--- linked-list-cycle/linked-list-cycle.java
+++ linked-list-cycle/linked-list-cycle.java
@@ -11,19 +11,14 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        
         if(head == null) return false;
-        
         ListNode slow = head;
         ListNode fast = head.next;
-        
-        while(slow != fast){
-            if(fast == null || fast.next == null) return false;
+        while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
-        } 
-        
-        return true;
-        
+            if(slow == fast) return true;
+        }
+        return false;
     }
 }

a1dfe9018f7fd93b6cdb02dbc8264a60c279b130
Christopher Coffee <chrisc1891@gmail.com>
2021-09-26 03:41:52 -0500

    Time: 168 ms (89.69%), Space: 36.4 MB (67.71%) - LeetHub

--- coin-change/coin-change.kt
+++ coin-change/coin-change.kt
@@ -1,18 +1,17 @@
 class Solution {
     fun coinChange(coins: IntArray, amount: Int): Int {
-        val dp = IntArray(amount+1) 
-        dp.fill(amount+1)
+        val dp = IntArray(amount + 1)
+        dp.fill(amount + 1)
         dp[0] = 0
         
         for(i in 0..amount){
             for(j in 0 until coins.size){
                 if(coins[j] <= i){
-                    dp[i] = Math.min(dp[i], 1 + dp[i - coins[j] ] );
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1 )
                 }
             }
         }
         
-        return if(dp[amount] > amount) -1 else dp[amount];
-        
+        return if(dp[amount] > amount) -1 else dp[amount]
     }
 }

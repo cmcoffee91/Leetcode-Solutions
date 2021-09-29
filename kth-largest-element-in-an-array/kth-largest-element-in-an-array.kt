7265a249e719f306c30c25eb31e136e5025eef4f
Christopher Coffee <chrisc1891@gmail.com>
2021-09-29 01:32:19 -0500

    Time: 176 ms (99.20%), Space: 37.6 MB (32.98%) - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.kt
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.kt
@@ -1,9 +1,19 @@
 class Solution {
     fun findKthLargest(nums: IntArray, k: Int): Int {
-        //[3,2,1,5,6,4] k=2
-        // pq   5 6
+        //nums.sort()
+       // return nums[nums.size-k]
         
-        nums.sort()
-        return nums[nums.size - k]
+        //Input: nums = [3,2,1,5,6,4], k = 2
+        //Output: 5
+        
+        //pq    5 6 
+        
+        val pq = PriorityQueue<Int>()
+        for(num in nums){
+            pq.add(num)
+            if(pq.size > k) pq.poll()
+        }
+        return pq.poll()
     }
+    
 }

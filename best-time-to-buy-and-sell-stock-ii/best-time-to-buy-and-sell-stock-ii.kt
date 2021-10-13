817ae411394899ac0e314c887b5908e5e5009d10
Christopher Coffee <chrisc1891@gmail.com>
2021-10-13 06:30:20 -0500

    Time: 290 ms (24.23%), Space: 41.8 MB (5.46%) - LeetHub

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.kt
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.kt
@@ -1,13 +1,11 @@
 class Solution {
     fun maxProfit(prices: IntArray): Int {
-        
         var max = 0
         for(i in 1 until prices.size){
-            val left = prices[i-1]
-            val right = prices[i]
-            if(right > left) max += (right - left) 
+            val current = prices[i]
+            val prev = prices[i-1]
+            if(current > prev) max += (current - prev)
         }
         return max
-        
     }
 }

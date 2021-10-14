11b9f099579a4147b8c2133f56498bb60b958cf7
Christopher Coffee <chrisc1891@gmail.com>
2021-10-14 02:56:47 -0500

    Time: 502 ms (42.64%), Space: 72 MB (18.02%) - LeetHub

--- rotate-array/rotate-array.kt
+++ rotate-array/rotate-array.kt
@@ -1,15 +1,27 @@
 class Solution {
     fun rotate(nums: IntArray, k: Int): Unit {
-        val map =  HashMap<Int, Int>()
-        nums.forEachIndexed{ index, value ->
-              map.put((index+k) % nums.size, value)
+        
+        
+        var rotateCount = k % nums.size
+        reverseArray(0, nums.size - 1, nums)
+        reverseArray(0, rotateCount - 1, nums)
+        reverseArray(rotateCount, nums.size - 1, nums)
+        
+    }
+    
+    
+    fun reverseArray(startIndex: Int, endIndex: Int, nums: IntArray){
+        
+        var left = startIndex
+        var right = endIndex 
+        
+        while(left < right){
+            var first = nums[left]
+            var second = nums[right]
+            nums[left++] = second
+            nums[right--] = first
         }
         
-        nums.forEachIndexed{ index, value ->
-            map[index]?.let{
-                nums[index] = it
-            }
-              
-        }
     }
+    
 }

7c96d58264dd57eb89d0874e68cdce5824fb986a
Christopher Coffee <chrisc1891@gmail.com>
2021-10-14 04:13:28 -0500

    Time: 262 ms (20.94%), Space: 34.9 MB (61.58%) - LeetHub

--- plus-one/plus-one.kt
+++ plus-one/plus-one.kt
@@ -1,20 +1,27 @@
 class Solution {
     fun plusOne(digits: IntArray): IntArray {
-       var carry = 1
-       for(i in digits.size-1 downTo 0){
-           var current = digits[i]
-           current += carry
-           carry = current / 10
-           digits[i] = current % 10
-       } 
-       if(carry > 0){
-           var increasedArray = IntArray(digits.size+1)
-           increasedArray[0] = carry
-           for(i in 1..increasedArray.size-1){
-               increasedArray[i] = digits[i-1]
-           }
-           return increasedArray
-       }
-       return digits
+        
+        var carry = 1
+        for(i in digits.size-1 downTo 0){
+            
+            val current = digits[i] 
+            val sum = current + carry
+            carry = sum / 10
+            digits[i] = sum % 10
+            
+        }
+        
+        if(carry > 0){
+            val increasedArray = IntArray(digits.size + 1)
+            increasedArray[0] = carry
+            for(i in 0 until digits.size){
+                increasedArray[i+1] = digits[i]
+            }
+            
+            return increasedArray
+        }
+        
+        return digits
+        
     }
 }

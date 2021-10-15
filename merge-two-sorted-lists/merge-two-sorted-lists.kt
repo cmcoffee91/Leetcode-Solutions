9782470608ae856b240b424959b5e07656f9098d
Christopher Coffee <chrisc1891@gmail.com>
2021-10-15 00:30:19 -0500

    Time: 225 ms (35.37%), Space: 35.8 MB (32.15%) - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.kt
+++ merge-two-sorted-lists/merge-two-sorted-lists.kt
@@ -10,29 +10,31 @@
 class Solution {
     fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
         
-        val dummy = ListNode(-1)
         
+    
+        
+        val dummy = ListNode(-1)
         var merged = dummy
         
-        var one = l1
-        var two = l2
+        var tmpOne = l1
+        var tmpTwo = l2
         
-        while(one != null && two != null){
-            if(one.`val` < two.`val`){
-                merged.next = one
-                one = one.next
-            }
-            else{
-                merged.next = two
-                two = two.next
-            }
-            merged = merged.next
+        while(tmpOne != null && tmpTwo != null){
+           if(tmpOne.`val` > tmpTwo.`val`){
+               merged.next = tmpTwo
+               tmpTwo = tmpTwo?.next
+           } 
+           else{
+               merged.next = tmpOne
+               tmpOne = tmpOne?.next
+           }
+           merged = merged.next
         }
         
-        merged.next = if(one != null) one else two
+        merged.next = if(tmpOne != null) tmpOne else tmpTwo 
+       
         
         return dummy.next
-    
         
     }
 }

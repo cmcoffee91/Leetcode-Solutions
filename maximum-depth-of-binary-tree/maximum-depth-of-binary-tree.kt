ab123d4e29e11de4994fac626be155e0c9f4b168
Christopher Coffee <chrisc1891@gmail.com>
2021-10-15 01:17:48 -0500

    Time: 378 ms (5.09%), Space: 38.3 MB (15.82%) - LeetHub

--- maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.kt
+++ maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.kt
@@ -11,9 +11,8 @@
 class Solution {
     fun maxDepth(root: TreeNode?): Int {
         if(root == null) return 0
-        val left = maxDepth(root.left)
-        val right = maxDepth(root.right)
-        
-        return Math.max(left, right) + 1
+        val left = maxDepth(root.left) + 1
+        val right = maxDepth(root.right) + 1
+        return Math.max(left, right) 
     }
 }

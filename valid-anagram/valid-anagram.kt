500a273550647805d6d681e30a30234be84db6f3
Christopher Coffee <chrisc1891@gmail.com>
2021-11-19 02:20:20 -0600

    Time: 192 ms (78.22%), Space: 36.4 MB (47.56%) - LeetHub

--- valid-anagram/valid-anagram.kt
+++ valid-anagram/valid-anagram.kt
@@ -3,24 +3,20 @@ class Solution {
         
         if(s.length != t.length) return false
         
-        val map = mutableMapOf<Char, Int>()
+        val map = HashMap<Char, Int>()
         
         val sArray = s.toCharArray()
         val tArray = t.toCharArray()
         
-        for(c in sArray){
-            map[c] = map.getOrDefault(c, 0) + 1
-        }
+        for(c in sArray) map[c] = map.getOrDefault(c, 0) + 1
         
         for(c in tArray){
-            var current =  map.getOrDefault(c, 0)
+            var current = map.getOrDefault(c, 0)
             if(current < 1) return false
             map[c] = --current
         }
         
-        
         return true
-       
+        
     }
-    
 }

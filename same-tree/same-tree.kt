468d16b4397406f49fbc1fa83ffa0407ac8cec76
Christopher Coffee <chrisc1891@gmail.com>
2021-12-03 01:52:51 -0600

    Time: 128 ms (99.53%), Space: 33.3 MB (91.12%) - LeetHub

--- same-tree/same-tree.kt
+++ same-tree/same-tree.kt
@@ -11,9 +11,9 @@
 class Solution {
     fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
         val queue = LinkedList<TreeNode?>()
+        
         queue.offer(p)
         queue.offer(q)
-        
         
         while(queue.size > 0){
             val one = queue.poll()
@@ -21,14 +21,16 @@ class Solution {
             
             if(one == null && two == null) continue
             if(one == null || two == null) return false
-            if(one?.`val` !=  two?.`val`) return false
+            if(one.`val` != two.`val`) return false
+            
             
             queue.offer(one?.left)
             queue.offer(two?.left)
             queue.offer(one?.right)
             queue.offer(two?.right)
+           
+            
         }
-        
         
         return true
     }

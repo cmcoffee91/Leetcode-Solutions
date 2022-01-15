1833698ca59b6bf3044adc4677ea0fc7d992fada
Christopher Coffee <chrisc1891@gmail.com>
2022-01-15 09:16:01 -0600

    Time: 187 ms (42.46%), Space: 34.9 MB (40.47%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.kt
@@ -0,0 +1,23 @@
+class Solution {
+    fun isValid(s: String): Boolean {
+        val sArray = s.toCharArray()
+        val map = HashMap<Char, Char>()
+        val stack = Stack<Char>()
+        
+        map['}'] = '{'
+        map[')'] = '('
+        map[']'] = '['
+        
+        sArray.forEach{ 
+            if(map.containsKey(it)){
+                if(stack.isEmpty() || map[it] != stack.pop()) return false
+            }
+            else{
+                stack.push(it)
+            }
+        }
+        if(stack.isNotEmpty()) return false
+        return true
+        
+    }
+}

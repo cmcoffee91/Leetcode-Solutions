bee44a6f7a9792b32b81219709abe30a4108193b
Christopher Coffee <chrisc1891@gmail.com>
2022-02-17 23:28:02 -0600

    Time: 430 ms (12.80%), Space: 44.4 MB (16.11%) - LeetHub

---
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.kt
@@ -0,0 +1,25 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun kthSmallest(root: TreeNode?, k: Int): Int {
+        val list = ArrayList<Int>()
+        inorderTraversal(list, root) 
+        return list[k-1]
+    }
+    
+    fun inorderTraversal(list: ArrayList<Int>, root: TreeNode?){
+        if(root == null) return
+        
+        inorderTraversal(list, root.left)
+        list.add(root.`val`)
+        inorderTraversal(list, root.right)
+    }
+}
